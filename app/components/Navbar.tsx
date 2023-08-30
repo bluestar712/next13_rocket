23f3866df7b0f11a1f77f3635add7a9d9ee129b0
bluestar712 <anatolii.blashkiv@outlook.com>
2023-08-30 17:55:31 -0400

    change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav className="bg-slate-600 p-4 flex justify-between flex-row sticky top-0 drop-shadow-xl">
         <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-            <Link href="/">WikiSearch!</Link>
+            <Link href="/">WikiRocket!</Link>
         </h1>
         <Search/>
     </nav>
